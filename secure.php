5284ad4be727f481848f7f942c39d85b61847a2b
Mochalka <64541687+Mochalka@users.noreply.github.com>
2021-04-04 01:20:24 +0600

    Delete secure.php

--- secure.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once './autoload.php';
-if (!isset($_SESSION['role']) || isset($_POST['out'])) {
-  session_destroy();
-  header('Location: template/auth.php');
-  exit;
-}
